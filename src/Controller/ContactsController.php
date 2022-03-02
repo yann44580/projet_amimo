0b01fa1951f9f3157a3802db9692a48b8e4d26b7
yann44580 <93974718+yann44580@users.noreply.github.com>
2022-03-02 21:32:21 +0100

    contact

--- src/Controller/ContactsController.php
+++ src/Controller/ContactsController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contacts;
+use App\Form\ContactsType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactsController extends AbstractController
 {
-    /**
-     * @Route("/contacts", name="contacts")
+    // /**
+    //  * @Route("/contacts", name="contacts")
+    //  */
+    // public function index(): Response
+    // {
+    //     return $this->render('contacts/index.html.twig', [
+    //         'controller_name' => 'ContactsController',
+    //     ]);
+    // }
+
+     /**
+     * @Route("/contacts", name="contacts", methods={"GET", "POST"})
      */
-    public function index(): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('contacts/index.html.twig', [
-            'controller_name' => 'ContactsController',
+        $contact = new Contacts();
+        $form = $this->createForm(ContactsType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('contacts/index.html.twig', [
+            'contact' => $contact,
+            'form' => $form,
         ]);
     }
 }
--- src/Form/ContactsType.php
+++ src/Form/ContactsType.php
@@ -6,18 +6,35 @@ use App\Entity\Contacts;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('contact_content')
-            ->add('contact_subject')
-            ->add('contact_date')
-            ->add('contact_email')
-            ->add('contact_fullname')
-            ->add('contact_firstname')
+            ->add('contact_content', TextareaType::class, [
+                'label' => 'Votre message'
+            ])
+            ->add('contact_subject', TextType::class, [
+                'label' => 'sujet'
+            ])
+            // ->add('contact_date', DateType::class, [
+            //     'widget' => 'single_text',
+            //     ''
+            // ])
+            ->add('contact_email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('contact_fullname', TextType::class, [
+                'label' => 'Nom de famille'
+            ])
+            ->add('contact_firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
         ;
     }
 
