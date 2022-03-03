c5c8b99614b88c411c5c1a4cf90c99028b597888
yann44580 <93974718+yann44580@users.noreply.github.com>
2022-03-03 21:52:34 +0100

    carousel

--- public/js/script.js
+++ public/js/script.js
@@ -5,24 +5,11 @@ $(document).ready(function () {
   $('select').formSelect();
   $('.datepicker').datepicker();
   $('#presentation_picture').fadeIn(3600);
-  // $('#owl-1 .owl-carousel').owlCarousel(
-  //   {
-  //     items: 5,
-  //     margin: 16,
-  //     stagePadding: 32,
-  //     loop: true,
-  //     autoplay: true,
-  //     autoplaySpeed: 500,
-  //     navSpeed: 500,
-  //     dots: false,
-  //     dotsEach: true,
-  //     nav: true,
-  //     // navText: ['Back','Next'],
-  //     navText: ['',''],
-  //     autoplayTimeout: 2000,
-  //     autoplayHoverPause: false,
-  //     animateOut: 'slideOutDown',
-  //     animateIn: 'flipInX',
-  //   }
-  // );
+  $('.carousel').carousel(
+    {
+      dist:0,
+      shift:0,
+      padding:20,
+    }
+  );
 });
